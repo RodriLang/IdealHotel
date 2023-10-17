b163461422a55522140d44e948cbb3a97cd3dda8
Romancastro99 <juanromancastroarce@gmail.com>
2023-10-17 18:48:40 -0300

    ReservaData

--- src/AccesoADatos/ReservaData.java
+++ src/AccesoADatos/ReservaData.java
@@ -10,7 +10,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import javax.swing.JOptionPane;
 
@@ -25,8 +28,20 @@ public class ReservaData {
         this.habData = habData;
     }
     
-    public void guardarReserva(Reserva res,HuespedData huesData, HabitacionData habData){
-     String sql="INSERT INTO reserva (idHuesped,idHabitacion,cantPax,fechaInn,fechaOut,importe) "
+    public void guardarReserva(Reserva res){
+        List<LocalDate> fechas=obtenerFechas(res.getFechaInn(), res.getFechaOut());
+        Boolean puede=true;
+        System.out.println(fechas);
+        for (LocalDate fecha : fechas) {
+            for (Reserva reserva : buscarReservaPorHabitacion(res.getHabitacion().getIdHabitacion(), huesData, habData)) {
+             if (fecha.equals(reserva.getFechaInn())||fecha.equals(reserva.getFechaOut())) {
+               puede = false;
+               break;
+            }   
+            }           
+        }     
+        if (puede) {       
+        String sql="INSERT INTO reserva (idHuesped,idHabitacion,cantPax,fechaInn,fechaOut,importe) "
             + "VALUES (?,?,?,?,?,?)";       
         try {
             PreparedStatement ps= con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
@@ -34,24 +49,23 @@ public class ReservaData {
             ps.setInt(2, res.getHabitacion().getIdHabitacion());
             ps.setInt(3, res.getCantPax());
             ps.setDate(4,Date.valueOf(res.getFechaInn()));
-            ps.setDate(5,Date.valueOf(res.getFechaOut())); 
+            ps.setDate(5,Date.valueOf(res.getFechaOut()));
             ps.setInt(6, res.getImporte());
             ps.executeUpdate();
             ResultSet rs=ps.getGeneratedKeys();
             if (rs.next()) {
                 res.setIdReserva(rs.getInt(1));
-                JOptionPane.showMessageDialog(null, "Reserva Guardada");
-                huesData.buscarHuespedId(res.getHuesped().getIdHuesped()).setAlojado(true);
-                habData.buscarHabitacion(res.getHabitacion().getIdHabitacion()).setOcupada(true);
-                ps.close();
+                JOptionPane.showMessageDialog(null, "Reserva Guardada");               
             }
-            
+            ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al trabajar en guardarReserva "+ex.getMessage());
         }    
+    }else{
+            System.out.println("No se puede reserva ");}
     }
     
-    public void eliminarReserva(int idHuesped, int idHabitacion, HuespedData huesData, HabitacionData habData){
+    public void eliminarReserva(int idHuesped, int idHabitacion){
     String sql="DELETE FROM `reserva` WHERE idHuesped=? AND idHabitacion=?";
     
         try {
@@ -59,9 +73,7 @@ public class ReservaData {
             ps.setInt(1, idHuesped);
             ps.setInt(2, idHabitacion);
             int exito=ps.executeUpdate();
-            if(exito==1){
-                huesData.buscarHuespedId(idHuesped).setAlojado(false);
-                habData.buscarHabitacion(idHabitacion).setOcupada(false);       
+            if(exito==1){       
                 JOptionPane.showMessageDialog(null, "Reserva eliminada");
             }else{
                   JOptionPane.showMessageDialog(null, "Reserva no encontrada");  
@@ -72,18 +84,18 @@ public class ReservaData {
              JOptionPane.showMessageDialog(null, "Error al trabajar con eliminarReserva"+ ex.getMessage());
         }
     }
-    public void modificarReserva(int cantPax, LocalDate fechaInn,LocalDate fechaOut,int importe, int idHuesped, int idHabitacion){
+    public void modificarReserva(Reserva res){
     
-        String sql="UPDATE reserva SET cantPax=?,fechaInn=?,fechaOut=?,importe=? WHERE idHuesped=? AND idHabitacion=?";
-    
+        String sql="UPDATE reserva SET cantPax=?,fechaInn=?,fechaOut=?,importe=? ,idHuesped=?,idHabitacion=? WHERE idReserva=?";    
         try {
             PreparedStatement ps=con.prepareStatement(sql);
-            ps.setInt(1, cantPax);
-            ps.setDate(2, Date.valueOf(fechaInn));
-            ps.setDate(3, Date.valueOf(fechaOut));
-            ps.setInt(4, importe);
-            ps.setInt(5, idHuesped);
-            ps.setInt(6, idHabitacion);
+            ps.setInt(1, res.getCantPax());
+            ps.setDate(2, Date.valueOf(res.getFechaInn()));
+            ps.setDate(3, Date.valueOf(res.getFechaOut()));
+            ps.setInt(4, res.getImporte());
+            ps.setInt(5, res.getHuesped().getIdHuesped());
+            ps.setInt(6, res.getHabitacion().getIdHabitacion());
+            ps.setInt(7, res.getIdReserva());
             int exito=ps.executeUpdate();
             if(exito==1){
                 JOptionPane.showMessageDialog(null, "Reserva modificada exitosamente");
@@ -97,7 +109,7 @@ public class ReservaData {
         }
     }
     
-    public List <Reserva> buscarReservaPorHuesped(int idHuesped, HuespedData huesData, HabitacionData habData){
+    public List <Reserva> buscarReservaPorHuesped(int idHuesped){
     
         List<Reserva> reserva=new ArrayList<Reserva>();
         String sql = "SELECT idReserva,idHuesped, idHabitacion, cantPax, fechaInn, fechaOut, importe FROM reserva WHERE  idHuesped=?";
@@ -106,8 +118,7 @@ public class ReservaData {
         try {
             ps = con.prepareStatement(sql);
             ps.setInt(1, idHuesped);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
+            ResultSet rs = ps.executeQuery();           
                 while (rs.next()) {                    
                 res = new Reserva();
                 res.setIdReserva(rs.getInt("idReserva"));
@@ -119,16 +130,14 @@ public class ReservaData {
                 res.setImporte(rs.getInt("importe")); 
                 reserva.add(res);               
                 } 
-            } else {
-                JOptionPane.showMessageDialog(null, "No se encuentra una reserva realizada por: "+huesData.buscarHuespedId(idHuesped).toString());
-            }
+                ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error en el método buscarReservaPorHuesped" + ex.getMessage());
         }
         return reserva;   
     }
     
-    public List<Reserva> buscarReservaPorFecha(LocalDate fecha, HuespedData huesData, HabitacionData habData){
+    public List<Reserva> buscarReservaPorFecha(LocalDate fecha){
     
         List<Reserva>reserva=new ArrayList<>();
         String sql = "SELECT * FROM `reserva` WHERE ? BETWEEN fechaInn AND fechaOut;";
@@ -138,7 +147,6 @@ public class ReservaData {
             ps = con.prepareStatement(sql);
             ps.setDate(1, Date.valueOf(fecha));
             ResultSet rs = ps.executeQuery();
-            if(rs.next()) {
                 while(rs.next()){
                 res = new Reserva();
                 res.setIdReserva(rs.getInt("idReserva"));
@@ -150,15 +158,40 @@ public class ReservaData {
                 res.setImporte(rs.getInt("importe"));
                 reserva.add(res);
                 }
-            } else {
-                JOptionPane.showMessageDialog(null, "No se encuentra una reserva realizada en esta fecha.");
-            }
+                ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error en el método buscarReservaPorFecha" + ex.getMessage());
         }
         return reserva;   
     }
     
+     public List <Reserva> buscarReservaPorHabitacion(int idHabitacion){
+    
+        List<Reserva> reserva=new ArrayList<Reserva>();
+        String sql = "SELECT idReserva,idHuesped, idHabitacion, cantPax, fechaInn, fechaOut, importe FROM reserva WHERE  idHabitacion=?";
+        Reserva res = null;
+        PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, idHabitacion);
+            ResultSet rs = ps.executeQuery();           
+                while (rs.next()) {                    
+                res = new Reserva();
+                res.setIdReserva(rs.getInt("idReserva"));
+                res.setHuesped(huesData.buscarHuespedId(rs.getInt("idHuesped")));
+                res.setHabitacion(habData.buscarHabitacion(idHabitacion));
+                res.setCantPax(rs.getInt("cantPax"));
+                res.setFechaInn(rs.getDate("fechaInn").toLocalDate());
+                res.setFechaOut(rs.getDate("fechaOut").toLocalDate()); 
+                res.setImporte(rs.getInt("importe")); 
+                reserva.add(res);               
+                } 
+                ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error en el método buscarReservaPorHabitacion" + ex.getMessage());
+        }
+        return reserva;   
+    }
     public List<Reserva> listarReservas(){
         List<Reserva>reserva=new ArrayList<>();
         String sql = "SELECT * FROM `reserva`";
@@ -167,7 +200,6 @@ public class ReservaData {
         try {
             ps = con.prepareStatement(sql);
             ResultSet rs = ps.executeQuery();
-            if(rs.next()) {
                 while(rs.next()){
                 res = new Reserva();
                 res.setIdReserva(rs.getInt("idReserva"));
@@ -179,15 +211,103 @@ public class ReservaData {
                 res.setImporte(rs.getInt("importe"));
                 reserva.add(res);
                 }
-            } else {
-                JOptionPane.showMessageDialog(null, "No se encuentran reservas");
-            }
+                ps.close();
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error en el método listarReserva" + ex.getMessage());
         }
         return reserva;   
     }
     
-    
-    }
+    public List<Habitacion> habitacionesDisponibles(LocalDate fecha1,LocalDate fecha2){
+    List<LocalDate> fechas=obtenerFechas(fecha1, fecha2);
+    List<Habitacion> habitaciones = habData.listarHabitaciones();
+    List<Reserva> ocupadas = new ArrayList<>();
+     for (LocalDate fecha : fechas) {
+         for (Reserva ocupada : buscarReservaPorFecha(fecha, huesData, habData)) {
+             ocupadas.add(ocupada);
+         }        
+     }
+    List<Habitacion> habitacionesDisponibles = new ArrayList<>();
 
+    for (Habitacion habitacion : habitaciones) {
+        boolean estaOcupada = false;
+        for (Reserva reserva : ocupadas) {
+            if (habitacion.getIdHabitacion() == reserva.getHabitacion().getIdHabitacion()) {
+                estaOcupada = true;
+                break;
+            }
+        }
+        if (!estaOcupada) {
+            habitacionesDisponibles.add(habitacion);
+        }
+    }
+    return habitacionesDisponibles;
+}  
+ public List<LocalDate> obtenerFechas(LocalDate fecha1,LocalDate fecha2){
+ 
+     List<LocalDate> fechas=new ArrayList<>();
+        int dias = (int) ChronoUnit.DAYS.between(fecha1, fecha2);
+        for (int i = 0; i < dias; i++) {
+            fechas.add(fecha1.plusDays(i));
+        } 
+        return fechas;
+ }
+ public int buscarIdReservasPorIDHabitacionyFecha(LocalDate fecha, int ID){
+        int idReserva=0;
+        String sql = "SELECT idReserva FROM `reserva` WHERE ? BETWEEN fechaInn AND fechaOut AND idHabitacion=?";
+        PreparedStatement ps;
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setDate(1, Date.valueOf(fecha));
+            ps.setInt(2,ID);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                idReserva = rs.getInt("idReserva");
+                System.out.println(idReserva + "-ok");
+            } else {
+                idReserva=0;
+                System.out.println(idReserva+"-Casi ok");
+            }
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error en el método listarReservaPorIDyPorFecha" + ex.getMessage());
+        }catch (NullPointerException ex){
+             System.out.println(idReserva+"- no ok");
+        }
+       
+        return idReserva;
+ }
+ 
+ public Reserva buscarReservaId(int idReserva){
+    
+        Reserva reserva=new Reserva();
+        String sql = "SELECT idReserva,idHuesped, idHabitacion, cantPax, fechaInn, fechaOut, importe FROM reserva WHERE  idReserva=?";
+        Reserva res = null;
+        PreparedStatement ps;
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, idReserva);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                while (rs.next()) {                    
+                res = new Reserva();
+                res.setIdReserva(rs.getInt("idReserva"));
+                res.setHuesped(huesData.buscarHuespedId(rs.getInt("idHuesped")));
+                res.setHabitacion(habData.buscarHabitacion(rs.getInt("idHabitacion")));
+                res.setCantPax(rs.getInt("cantPax"));
+                res.setFechaInn(rs.getDate("fechaInn").toLocalDate());
+                res.setFechaOut(rs.getDate("fechaOut").toLocalDate()); 
+                res.setImporte(rs.getInt("importe")); 
+                reserva=res;            
+                } 
+            } else {
+                JOptionPane.showMessageDialog(null, "No se encuentra una reserva con el Id seleccionado");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error en el método buscarReservaPorId" + ex.getMessage());
+        }
+        return reserva;   
+        }            
+} //Seguir con la lista de fechas y ponerlas en los metodos!
+   
